8f3e07f4858c64ca09ceca75f2d3d82584e5117e
Bhaviya Babu <bhaviya.babu@ratnaglobaltech.com>
2023-04-28 16:47:38 +0530

    updated changes

--- myshop/settings.py
+++ myshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n63!e9m^c-!u0uuuot-+g#)2_dl+xmdm-xra8qntjqs@n=b$oi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
